1385b7699cba40ec4ceed050422d56b00912f148
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2019-11-19 09:01:50 +0900

    Add spring-boot profile configuration

---
+++ src/main/java/io/github/hanjoongcho/SpringbootTestbedApplication.java
@@ -0,0 +1,13 @@
+package io.github.hanjoongcho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestbedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/hanjoongcho/SpringbootTestbedApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hanjoongcho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
