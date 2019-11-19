d037479c7c326885d8b3883bc0f32d1a8ac73a95
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2019-11-19 13:41:20 +0900

    Add ckeditor image upload sample code.

--- src/main/java/io/github/hanjoongcho/ckeditor/CKEditorController.java
+++ src/main/java/io/github/hanjoongcho/ckeditor/CKEditorController.java
@@ -1,17 +1,23 @@
 package io.github.hanjoongcho.ckeditor;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +26,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class CKEditorController {
-
-    private String ckeditorStorageImagePath = "C:/temp";
     private Logger logger = LoggerFactory.getLogger(CKEditorController.class);
+    
+    @Value("${ckeditor.storage.image-path}")
+    private String ckeditorStorageImagePath;
+    
+    @Value("${ckeditor.storage.service-url:}")
+    private String ckeditorStorageServiceUrl;
     
     @CrossOrigin
     @RequestMapping(value = "/upload/image", method = RequestMethod.POST)
@@ -33,24 +43,21 @@ public class CKEditorController {
         if (!file.isEmpty()) {
             try {
                 String fileClientName = file.getOriginalFilename();
-                String rootFilePath=ckeditorStorageImagePath+"\\images\\";
-
-                File rootFile=new File(rootFilePath);
+                logger.info(String.format("Original file name is %s", fileClientName));
+                String rootFilePath = ckeditorStorageImagePath;
+                File rootFile = new File(rootFilePath);
                 if (!rootFile.exists()){
                     rootFile.mkdir();
                 }
 
-                String pathName = rootFile +File.separator+ fileClientName;
-                File newfile = new File(pathName);
-                byte[] bytes = file.getBytes();
-                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(newfile));
-                stream.write(bytes);
-                stream.close();
-
+                int fileSequence = rootFile.listFiles().length + 1;
+                String pathName = rootFile + File.separator+ fileSequence;
+                file.transferTo(new File(pathName));
+                
                 // FIXME hard code result map
-                String fileUrl = "https://user-images.githubusercontent.com/156149/47722540-ed367c00-dc52-11e8-97d4-b7eafbb6e495.png";
-                resultMap.put("fileName", "sample01.PNG");
-                resultMap.put("uploaded", 1);
+                String fileUrl = ckeditorStorageServiceUrl + "/" + fileSequence;
+                resultMap.put("fileName", fileClientName);
+                resultMap.put("uploaded", 1); // success: 1, fail: 0
                 resultMap.put("url", fileUrl);
             } catch (Exception e) {
                 logger.info("You failed to upload " + name + " => " + e.getMessage());
@@ -59,5 +66,18 @@ public class CKEditorController {
             logger.info("You failed to upload " + name + " because the file was empty.");
         }
         return resultMap;
+    }
+    
+    /**
+     * FIXME: Return application mime type dynamically.
+     * 
+     * @param imageId
+     * @return
+     * @throws IOException
+     */
+    @GetMapping(value = "/image/{imageId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getImageWithMediaType(@PathVariable("imageId") String imageId) throws IOException {
+        InputStream is = FileUtils.openInputStream(new File(ckeditorStorageImagePath, imageId));
+        return IOUtils.toByteArray(is);
     }
 }
