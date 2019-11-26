54f871b46f97d13b06a796ff84974e104fb29113
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2019-11-27 07:03:56 +0900

    Add webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,36 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = (env) => {
     let clientPath = path.resolve(__dirname, 'src/main/client');
-    let outputPath = path.resolve(__dirname, 'out');
+    let outputPath = path.resolve(__dirname, (env == 'production') ? 'src/main/resources/static' : 'out')
 
     return {
         mode: !env ? 'development' : env,
         entry: {
+            vendors: [ 'jquery', 'moment' ],
             index: clientPath + '/index.js'
         },
         output: {
             path: outputPath,
             filename: '[name].js'
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+                cacheGroups: {
+                    vendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors'
+                    }
+                }
+            },
+            minimizer: (env == 'production') ? [
+                new UglifyJsPlugin(),
+                new OptimizeCssAssetsPlugin()
+            ] : []
         },
         devServer: {
             contentBase: outputPath,
@@ -23,6 +42,48 @@ module.exports = (env) => {
             },
             inline: true,
             hot: false
-        }
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: 'env'
+                    }
+                }]
+            }, {
+                test: /\.(css)$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader
+                }, {
+                    loader: 'css-loader'
+                }]
+            }, {
+                test: /\.(jpe?g|png|gif)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024 * 10 // 10kb
+                    }
+                }]
+            }, {
+                test: /\.(svg)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images/'
+                    }
+                }]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                path: outputPath,
+                filename: '[name].css'
+            })
+            // , new BundleAnalyzerPlugin()
+        ]
     }
 }
